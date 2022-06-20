465d02a6c46f9d19565ab268decf5e99c34e0a1d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-20 11:30:24 -0300

    Update Js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* navbar togglec*/
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElems = [overlay, navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElems.length; i++) {
+  navElems[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
